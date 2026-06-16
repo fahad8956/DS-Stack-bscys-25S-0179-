55bb6ba65ed8fc7c7df1def086d520240241d62f
fahad8956 <aleemfahad88@gmail.com>
2026-06-16 16:45:18 +0500

    Add decimalToBinary method in Main class
    Implement decimal to binary conversion using a stack.

---
+++ Main.java
@@ -0,0 +1,25 @@
+import java.util.Stack;
+public class Main {
+    public static void decimalToBinary(int num) {
+        if (num == 0) {
+            System.out.println("0");
+            return;
+        }
+        Stack<Integer> stack = new Stack<>();
+        int temp = num;
+        while (temp > 0) {
+            stack.push(temp % 2);
+            temp = temp / 2;
+        }
+        while (!stack.isEmpty()) {
+            System.out.print(stack.pop());
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        decimalToBinary(13);
+        decimalToBinary(50);
+        decimalToBinary(255);
+        decimalToBinary(1024);
+    }
+}
